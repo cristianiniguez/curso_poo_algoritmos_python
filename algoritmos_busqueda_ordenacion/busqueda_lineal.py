e48a55fb12e5770217094de9fb45e467e0a96bb2
cristianiniguez <felizpc1@gmail.com>
2021-01-30 17:34:13 -0400

    algoritmos de busqueda y ordenacion: busqueda lineal

---
+++ algoritmos_busqueda_ordenacion/busqueda_lineal.py
@@ -0,0 +1,23 @@
+import random
+
+
+def busqueda_lineal(lista, objetivo):
+    match = False
+
+    for elemento in lista:  # O(n)
+        if elemento == objetivo:
+            match = True
+            break
+
+    return match
+
+
+if __name__ == '__main__':
+    tamanho_de_lista = int(input('De que tamanho sera la lista?: '))
+    objetivo = int(input('Que numero quieres encontrar?: '))
+
+    lista = [random.randint(0, 100) for i in range(tamanho_de_lista)]
+    encontrado = busqueda_lineal(lista, objetivo)
+
+    print(lista)
+    print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
